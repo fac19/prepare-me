7c2448da734b755aba2c3e96fbe2ced84b585ffe
Roger Heathcote <git@technicalbloke.com>
2020-06-08 21:59:06 +0100

    Started StoryPage componenet and added Starting School story template. Relates #25
    Co-authored-by: akomiqaia <akaki.mikaia.1@iliauni.edu.ge>

--- src/pages/StoryPage.js
+++ src/pages/StoryPage.js
@@ -6,6 +6,8 @@ const StoryPage = () => {
   const pageNumber = params.pageNumber;
   const [state, dispatch] = useContext(SiteState);
   const fields = state.pages[pageNumber - 1].fields;
+  // get template name from state
+  // get the component, as ThePage, by it's name
 
   return (
     <>
