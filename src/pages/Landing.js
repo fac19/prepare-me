e4ac3fd8b2764542e94a6f2b4fa56e418b634120
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-06-08 12:36:27 +0100

    Setup router, state and theming Relates #5
    Co-authored-by: chloeh24 <chloeh24@hotmail.com>
    
    Co-authored-by: Alexreid95 <alexreid95@gmail.com>
    
    Co-authored-by: Roger-Heathcote <[github@technicalbloke.com](mailto:github@technicalbloke.com)>

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Grid, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import PrepareMeLogo from "../static/pm.png";
 
 function LandingPage() {
-  return <div>Welcome to our storybuilder!</div>;
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <image src={PrepareMeLogo}></image>
+      </Grid>
+      <Grid item>
+        <h1>Welcome to our storybuilder!</h1>
+        <span>What would you like to build today?</span>
+      </Grid>
+      <Grid item>
+        <Button component={Link} to="/select-story-template">
+          PICTURE STORY
+        </Button>
+      </Grid>
+      <Grid item></Grid>
+    </Grid>
+  );
 }
 
 export default LandingPage;
