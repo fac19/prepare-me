6dc9ab499365273c0a6ff229957b12f2e27c8264
Roger Heathcote <git@technicalbloke.com>
2020-06-08 16:33:04 +0100

    Make landing page. Relates #21
    Co-authored-by: akomiqaia <akaki.mikaia.1@iliauni.edu.ge>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useReducer } from "react";
-import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
-import { StylesProvider } from "@material-ui/core/styles";
-import { CssBaseline } from "@material-ui/core";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, {useReducer} from "react";
+import {ThemeProvider as MuiThemeProvider} from "@material-ui/core/styles";
+import {StylesProvider} from "@material-ui/core/styles";
+import {CssBaseline} from "@material-ui/core";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import "./App.css";
 import theme from "./theme";
@@ -11,24 +11,24 @@ import initialState from "./model/initialState";
 import updateState from "./model/updateState";
 
 // Pages
-import LandingPage from "./pages/Landing";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  const [state, dispatch] = useReducer(updateState, initialState);
-  return (
-    <StylesProvider injectFirst>
-      <MuiThemeProvider theme={theme}>
-        <CssBaseline />
-        <SiteContext.Provider value={[state, dispatch]}>
-          <Router>
-            <Switch>
-              <Route path="/" component={LandingPage} />
-            </Switch>
-          </Router>
-        </SiteContext.Provider>
-      </MuiThemeProvider>
-    </StylesProvider>
-  );
+    const [state, dispatch] = useReducer(updateState, initialState);
+    return (
+        <StylesProvider injectFirst>
+            <MuiThemeProvider theme={theme}>
+                <CssBaseline />
+                <SiteContext.Provider value={[state, dispatch]}>
+                    <Router>
+                        <Switch>
+                            <Route path="/" component={LandingPage} />
+                        </Switch>
+                    </Router>
+                </SiteContext.Provider>
+            </MuiThemeProvider>
+        </StylesProvider>
+    );
 }
 
 export default App;
