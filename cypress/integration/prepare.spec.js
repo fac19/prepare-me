51d07b9df8bab8e6908d96cf76a32072f88b9e08
AlexReid95 <alexreid95@gmail.com>
2020-06-10 23:35:58 +0100

    First cypress test Relates #47

--- cypress/integration/prepare.spec.js
+++ cypress/integration/prepare.spec.js
@@ -2,6 +2,6 @@ describe('Prepare Me Builder', () => {
   it('should display prepare me website', () => {
     cy.viewport('iphone-6');
     cy.visit('http://localhost:3000/');
-    cy.title().should('include', 'WELCOME TO OUR STORY BUILDER');
+    cy.title().should('include', 'Prepare Me Builder');
   });
 });
