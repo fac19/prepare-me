fc7e44729e7a79606cd5d7c2632d294881037ff0
AlexReid95 <alexreid95@gmail.com>
2020-06-10 23:55:17 +0100

    Refactor base url and add a test Relates #47

--- cypress/integration/prepare.spec.js
+++ cypress/integration/prepare.spec.js
@@ -1,7 +1,17 @@
 describe('Prepare Me Builder', () => {
+  it('is working', () => {
+    expect(true).to.equal(true);
+  });
+
   it('should display prepare me website', () => {
     cy.viewport('iphone-6');
-    cy.visit('http://localhost:3000/');
+    cy.visit('/');
     cy.title().should('include', 'Prepare Me Builder');
+  });
+
+  it('should display story templates after clicking "PICTURE STORY', () => {
+    cy.visit('/');
+    cy.contains('PICTURE STORY').click();
+    cy.url().should('include', '/select-story-template');
   });
 });
