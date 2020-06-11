3510a2d81b5b461c84169ca699186ebf311fea8f
AlexReid95 <alexreid95@gmail.com>
2020-06-11 02:11:07 +0100

    Do more configuration of cypress and add test Relates #47

--- cypress/integration/prepare.spec.js
+++ cypress/integration/prepare.spec.js
@@ -1,17 +1,27 @@
-describe('Prepare Me Builder', () => {
-  it('is working', () => {
-    expect(true).to.equal(true);
-  });
-
+describe('Prepare.spec', () => {
   it('should display prepare me website', () => {
     cy.viewport('iphone-6');
     cy.visit('/');
     cy.title().should('include', 'Prepare Me Builder');
   });
 
-  it('should display story templates after clicking "PICTURE STORY', () => {
+  it('should display /select-story-template after clicking "PICTURE STORY', () => {
     cy.visit('/');
     cy.contains('PICTURE STORY').click();
     cy.url().should('include', '/select-story-template');
+  });
+
+  it('should display /story-page/1 after clicking "Blank Template"', () => {
+    cy.visit('/');
+    cy.contains('PICTURE STORY').click();
+    cy.contains('Blank Template').click();
+    cy.url().should('include', '/story-page/1');
+  });
+
+  it('should display /story-page/1 after clicking "School Template"', () => {
+    cy.visit('/');
+    cy.contains('PICTURE STORY').click();
+    cy.contains('School Template').click();
+    cy.url().should('include', '/story-page/1');
   });
 });
---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,5 @@
+describe('Sample.spec', () => {
+  it('is working', () => {
+    expect(true).to.equal(true);
+  });
+});
