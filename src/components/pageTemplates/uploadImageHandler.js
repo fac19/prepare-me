097f4e6561eca752a92abccae0bfab52f2ee6ecf
Chloe <chloeh24@hotmail.com>
2020-06-11 12:15:52 +0100

    add google api

--- src/components/pageTemplates/uploadImageHandler.js
+++ src/components/pageTemplates/uploadImageHandler.js
@@ -5,20 +5,37 @@ function uploadImageHandler(e, pageNumber, dispatch) {
     {
       cloudName: 'prepareme',
       uploadPreset: 'ru2kdjzc',
+      sources: ['local', 'url', 'image_search', 'google_drive', 'facebook'],
+      googleApiKey: 'AIzaSyCoy_NII_6Wssms4Z6GHLTyZVJVpgr4Cic',
+      // showAdvancedOptions: true,
       cropping: true,
-      sources: ['local', 'url', 'image_search', 'facebook', 'google_drive'],
-      // searchBySites: ['https://unsplash.com/'],
-      // googleApikey:
-      //   styles: {
-      //     palette: {
-      //   window: "#FFF",
-      //   windowBorder: "#90A0B3",
-      //   tabIcon: "#0E2F5A",
-      //   menuIcons: "#5A616A",
-      //   textDark: "#000000",
-      //   },
-      // },
+      multiple: false,
+      defaultSource: 'local',
+      styles: {
+        palette: {
+          window: '#F5F5F5',
+          sourceBg: '#FFFFFF',
+          windowBorder: '#90a0b3',
+          tabIcon: '#0094c7',
+          inactiveTabIcon: '#69778A',
+          menuIcons: '#0094C7',
+          link: '#53ad9d',
+          action: '#8F5DA5',
+          inProgress: '#0194c7',
+          complete: '#53ad9d',
+          error: '#c43737',
+          textDark: '#000000',
+          textLight: '#FFFFFF',
+        },
+        // fonts: {
+        //   'default': null,
+        //   "'Poppins', sans-serif": {
+        //     url: 'https://fonts.googleapis.com/css?family=Poppins',
+        //     active: true,
+        //   },
+      },
     },
+
     (error, result) => {
       if (!error && result && result.event === 'success') {
         console.log(result.info);
@@ -29,5 +46,4 @@ function uploadImageHandler(e, pageNumber, dispatch) {
     },
   );
 }
-
 export default uploadImageHandler;
