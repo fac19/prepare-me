a045b4a39420c7a4c4f46adbc3f9ebc3d13e9f72
AlexReid95 <alexreid95@gmail.com>
2020-06-15 12:41:33 +0100

    Add more inforamtion in the cover pages Relates #80
    Co-authored-by: chloeh24 <chloeh24@hotmail.com>

--- src/storyTemplates/blankTemplate.js
+++ src/storyTemplates/blankTemplate.js
@@ -3,9 +3,9 @@ const blankTemplate = {
     {
       pageTemplate: 'FrontCover',
       fields: {
-        text1: 'Change to your text',
+        text1: 'Click to add a title',
         pic1:
-          'https://1ce7fd4e-a-52c4923f-s-sites.googlegroups.com/a/univsul.edu.iq/lana-abdulhamid/academic-profile/pro_pic.jpg?attachauth=ANoY7cpMoBRWAarwEJ91xe1KfSX1aZ_kCtk6YRVT8e8qlU8vYAZnOyZECBM-x2a7kmDfrlIIJJAsI0vQdIUCTcvGRVkYmd3BLnOH3yCkXiNsJU5DvZEnSQ-4FKUSogvCREbnN0NO9S1dhT3wPvW41LXFit1J65iSUIHPRz3TSwKCjn8HqUnaVYkeaQsuf97BLglQ-H-Uk251ZQIWdSiLQwR-vKqroz7-5jzr-w7ohxKkmELNqGWYqIk%3D&attredirects=0',
+          'https://res.cloudinary.com/didur5psx/image/upload/v1591714780/starting_school/yourpic_rjemqq.jpg',
       },
     },
     {
