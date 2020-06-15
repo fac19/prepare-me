38b21414bdc034dee84ecd4584d37a082a31589a
AlexReid95 <alexreid95@gmail.com>
2020-06-15 11:57:55 +0100

    remove unused import
    Co-authored-by: chloeh24 <chloeh24@hotmail.com>

--- src/components/WarningPage.js
+++ src/components/WarningPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
