83ad1c6abd6d3b2628a79943e008a9bfac3c166b
Roger Heathcote <git@technicalbloke.com>
2020-06-18 09:30:51 +0100

    Fixed chrome jankiness where 100vh was larger than the actual viewport height on all pages but StoryPage. Relates #48 Relates #128

--- src/pages/ActionsPage.js
+++ src/pages/ActionsPage.js
@@ -6,9 +6,10 @@ import styled from 'styled-components';
 import BottomNavBar from '../components/BottomNavBar';
 import SiteContext from '../model/SiteContext';
 import { Swipeable } from 'react-swipeable';
+import Div100vh from 'react-div-100vh';
 
 const MyGrid = styled(Grid)`
-  height: 100vh;
+  height: 100%;
 `;
 
 function ActionsPage() {
@@ -23,40 +24,42 @@ function ActionsPage() {
 
   return (
     <Swipeable onSwipedRight={handleRightSwipe}>
-      <MyGrid
-        container
-        direction="column"
-        alignContent="stretch"
-        alignItems="center"
-        justify="space-evenly"
-      >
-        <Grid item>
-          <Typography variant="h1" align="center">
-            Looks like you're finished.
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="h2" align="center">
-            What would you like to do now?
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Button component={Link}>Print</Button>
-        </Grid>
-        <Grid item>
-          <Button component={Link}>Download</Button>
-        </Grid>
-        <Grid item>
-          <Button component={Link}>Read Mode</Button>
-        </Grid>
-        <Grid item>
-          <Button component={Link}>Start Over</Button>
-        </Grid>
-        <Grid item>
-          {/* <BottomNavBar {...{ pageNumber, totalPages }} /> */}
-          <BottomNavBar totalPages={totalPages} />
-        </Grid>
-      </MyGrid>
+      <Div100vh>
+        <MyGrid
+          container
+          direction="column"
+          alignContent="stretch"
+          alignItems="center"
+          justify="space-between"
+        >
+          <Grid item>
+            <Typography variant="h1" align="center">
+              Looks like you're finished.
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h2" align="center">
+              What would you like to do now?
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button component={Link}>Print</Button>
+          </Grid>
+          <Grid item>
+            <Button component={Link}>Download</Button>
+          </Grid>
+          <Grid item>
+            <Button component={Link}>Read Mode</Button>
+          </Grid>
+          <Grid item>
+            <Button component={Link}>Start Over</Button>
+          </Grid>
+          <Grid item>
+            {/* <BottomNavBar {...{ pageNumber, totalPages }} /> */}
+            <BottomNavBar totalPages={totalPages} />
+          </Grid>
+        </MyGrid>
+      </Div100vh>
     </Swipeable>
   );
 }
