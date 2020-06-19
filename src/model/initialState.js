55b08400c460be78a53310c7d3d1c878c888fe31
Roger Heathcote <git@technicalbloke.com>
2020-06-19 13:22:03 +0100

    Added some more comments to model folder files.

--- src/model/initialState.js
+++ src/model/initialState.js
@@ -1,3 +1,9 @@
+/*
+  This object hold the site's initial state. It is used to initialise the useReducer function in app.js. By default it just contains an empty array called pages although there is no reason more properties can't be added as required. 
+  
+  The pages array is where we keep page records, one for each page of the story. Page records are objects that contain the name of the page template and values for each field in that template. Examples of page records can be found in each of the story templates in the src/storyTemplates folder. Loading story templates (in updateState) is the process of copying their page records copied into the pages array.
+*/
+
 const initialState = {
   pages: [],
 };
