a314ab5fb013cab5b291d8030690a42a76341748
Roger Heathcote <git@technicalbloke.com>
2020-06-19 12:45:37 +0100

    Added better comments to the files in the model folder

--- src/model/allPages.js
+++ src/model/allPages.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import getPageTemplateByName from '../components/pageTemplates/getPageTemplateByName';
+
+/*
+   This file creates an array of all the pages in the story.
+   Each page is a page template component rendered with the appropriate fields.
+*/
 
 const OurPaper = styled.div`
   page-break-before: always;
-  /* width: 100%; */
   height: 90vh;
 `;
 
 function allPages(state) {
   const pages = [];
-  // const [state] = useContext(SiteContext);
   state.pages.forEach((item, index) => {
     // Get the fields for the page we are on
     const fields = item.fields;
@@ -20,6 +22,8 @@ function allPages(state) {
     const templateName = item.pageTemplate;
     const Page = getPageTemplateByName[templateName];
 
+    // Render each component with the right fields and
+    // push it onto the array.
     pages.push(
       <OurPaper key={index}>
         <Page fields={fields} />
